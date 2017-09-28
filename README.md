797da7305f1191504c5b120db17d1f6cfd8c472f
Hugh Morrison <hmorri32@gmail.com>
2017-09-28 08:22:40 -0600

    scrape example

---
+++ README.md
@@ -0,0 +1,8 @@
+#figure out how to grab that yung data 
+
+```js
+	//visit MSW http://magicseaweed.com/
+	//grab dropdown, if stay in north america - california, south. Seems to be default. 
+	//Select location.
+	//Scrape stuff. 
+```
---
+++ script.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({  show: true });
+	
+// nightmare
+// 	.viewport(1500,1500)
+//   .goto('http://magicseaweed.com/Lunada-Bay-Surf-Report/283/')
+//   .wait(500)
+//   .scrollTo(1220, 200)
+//   .wait(500)
+//   .evaluate(function() {
+
+//   	let table = document.querySelectorAll('div#msw-js-fc.msw-fc.msw-fc-us')
+
+//   	let timeStamp  = document.querySelectorAll('td.msw-fc-thour.msw-js-tooltip')
+//   	let waveHeight = document.querySelectorAll('td.msw-fc-fps.msw-fc-ps')
+
+//   	let tables = [].slice.call(waveHeight)
+//   	let waves  = [].slice.call(waveHeight)
+//   	let times  = [].slice.call(timeStamp)
+
+//   	let mapped = times.map(function(node){
+//   		return node.innerText
+//   	})
+
+//   	let mapped2 = waves.map(function(node) {
+//   		return node.innerText
+//   	})
+
+//   	return mapped
+//   })
+
+//   .end()
+//   .then(function (result) {
+//   	console.log(result)
+//   })
+//   .catch((error) => console.log(error))
+
+nightmare
+	.viewport(1500,1500)
+	.goto('http://beachgrit.com')
+	.wait(500)
+	.evaluate(function() {
+		let articles = document.querySelectorAll('article.digest.middle-line.flex')
+		let list     = [].slice.call(articles)
+		let article = document.querySelector('article.digest.middle-line.flex')
+
+		hrefs = list.map((article) => {
+			return article.children['0'].href.concat(article.children['0'].innerText).split('\n')
+		})
+
+		hrefs = [].concat.apply([], hrefs)
+		hrefs = hrefs.filter((n) => n != ''); 
+
+		return hrefs
+	})
+	.end()
+	.then(function(result) {
+		console.log(result)
+		fs.writeFileSync('output.json', JSON.stringify(result));		
+	})
+	.catch((error) => console.log(error))	
